ccdca1aba75dd35a40fe06d5f3a311ff5ad39ca9
Hardik Sheth <hardik.sheth1717@live.com>
2022-04-14 22:33:59 +0530

    Added input and output variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group_name" {
-  default     = "semaphore-rg"
+  default     = "default-rg"
   description = "Name of the resource group"
   type        = string
 }
 
 variable "azure_region" {
-  default     = "westindia"
+  default     = "default-region"
   description = "Region name of Azure DC"
   type        = string
 }
